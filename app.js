76880f515147a7f7348ee5d7425becc4634d5ec3
Julien08 <julienalex08@gmail.com>
2025-12-17 12:44:56 -0500

    trying to finish

--- app.js
+++ app.js
@@ -1,22 +1,58 @@
 /*-------------------------------- Constants --------------------------------*/
 
-const rows = document.querySelectorAll('.grid');
+const rows = document.querySelectorAll(".grid");
+console.log(rows);
 
 /*---------------------------- Variables (state) ----------------------------*/
 
 let currentRow = 0;
 let currentCol = 0;
 
-
 /*------------------------ Cached Element References ------------------------*/
 
-
-
 /*-------------------------------- Functions --------------------------------*/
+const handleKeyPress = (e) => {
+  const key = e.key;
+  console.log(key);
 
+  if (key === "Backspace") {
+    if (currentCol === 0) {
+      return;
+    }
+    currentCol--;
+
+    const row = rows[currentRow];
+    const box = row.children[currentCol];
+
+    box.textContent = "";
+    return;
+  }
+
+  if (key === "Enter") {
+    if (currentCol !== 5) return;
+
+    currentRow++;
+    currentCol = 0;
+    return;
+  }
+
+  if (key.length !== 1 || key < "a" || key > "z") {
+    return;
+  }
+
+  if (currentCol >= 5) {
+    return;
+  }
+
+  const row = rows[currentRow];
+  console.log(row.children);
+  const box = row.children[currentCol];
+
+  box.textContent = key;
+
+  currentCol++;
+};
 
 /*----------------------------- Event Listeners -----------------------------*/
 
-document.addEventListener('keydown', handleKeyPress);
-
-
+document.addEventListener("keydown", handleKeyPress);
