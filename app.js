1c9a908237e0dba2ceb00ba179b399fe9970bc88
Julien08 <julienalex08@gmail.com>
2025-12-17 11:05:44 -0500

    trying to finish

---
+++ app.js
@@ -0,0 +1,22 @@
+/*-------------------------------- Constants --------------------------------*/
+
+const rows = document.querySelectorAll('.grid');
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+let currentRow = 0;
+let currentCol = 0;
+
+
+/*------------------------ Cached Element References ------------------------*/
+
+
+
+/*-------------------------------- Functions --------------------------------*/
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+document.addEventListener('keydown', handleKeyPress);
+
+
