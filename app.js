335a9efab0017029cfe3a19145131a3050c36538
Julien08 <julienalex08@gmail.com>
2025-12-19 12:09:23 -0500

    almost done

--- app.js
+++ app.js
@@ -3,19 +3,23 @@ import { WORDS } from "./words.js";
 /*-------------------------------- Constants --------------------------------*/
 
 const rows = document.querySelectorAll(".grid");
+const messageEl = document.querySelector("#message");
 
 /*---------------------------- Variables (state) ----------------------------*/
 
 let currentRow = 0;
 let currentCol = 0;
+let isGameOver = false;
 
 /*------------------------ Cached Element References ------------------------*/
 const resetEl = document.querySelector("#button");
 /*-------------------------------- Functions --------------------------------*/
-const targetWord = WORDS[Math.floor(Math.random() * WORDS.length)];
+let targetWord = WORDS[Math.floor(Math.random() * WORDS.length)];
 
 const handleKeyPress = (e) => {
   const key = e.key;
+
+  if (isGameOver) return;
 
   if (key === "Backspace") {
     if (currentCol === 0) {
@@ -31,6 +35,7 @@ const handleKeyPress = (e) => {
   }
 
   if (key === "Enter") {
+    console.log("I'm in here");
     if (currentCol !== 5) return;
     const row = rows[currentRow];
 
@@ -72,10 +77,19 @@ const handleKeyPress = (e) => {
       box.classList.add(results[i]);
     }
 
-    // const guess = letters.join("");
+    const guess = letters.join("");
 
     currentRow++;
     currentCol = 0;
+
+    if (guess === targetWord) {
+      isGameOver = true;
+      messageEl.textContent = "You Win!!!";
+    } else if (currentRow >= 5) {
+      isGameOver = true;
+      messageEl.textContent = `You Lose!! The word you are looking for is ${targetWord}`;
+    }
+
     return;
   }
 
@@ -99,4 +113,18 @@ const handleKeyPress = (e) => {
 /*----------------------------- Event Listeners -----------------------------*/
 
 document.addEventListener("keydown", handleKeyPress);
-// resetEl.addEventListener("click");
+resetEl.addEventListener("click", () => {
+  currentRow = 0;
+  currentCol = 0;
+  isGameOver = false;
+
+  rows.forEach((row) => {
+    for (let box of row.children) {
+      box.classList.remove("empty", "wrong", "right");
+      box.textContent = "";
+    }
+  });
+
+  messageEl.textContent = "";
+  targetWord = WORDS[Math.floor(Math.random() * WORDS.length)];
+});
